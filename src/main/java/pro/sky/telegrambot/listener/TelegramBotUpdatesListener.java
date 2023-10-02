874f7d351c4b36c93f037c05da908b63d3fdcd77
IlyaAfanasev <gecktor.86@mail.ru>
2023-10-02 22:11:45 +0400

    add methods createStartMessage, sendMessage

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,18 +3,21 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.configuration.TelegramBotConfiguration;
 
 import javax.annotation.PostConstruct;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
-    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
     @Autowired
     private TelegramBot telegramBot;
@@ -27,10 +30,31 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
+
+            if (update.message().text().contains("start")) {
+                createStartMessage(update);
+            }
+
             logger.info("Processing update: {}", update);
             // Process your updates here
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+//метод для обработки команды "/start"
+    private void createStartMessage(Update update) {
+        Long chatId = update.message().chat().id();
+        String userName = update.message().chat().firstName();
+        String text = update.message().text();
+
+        SendMessage message = new SendMessage(chatId, text+ ". Hello " + userName);
+        sendMessage(message);
+
+    }
+//метод для отправки сообщения
+    private void sendMessage(SendMessage message) {
+        SendResponse response = telegramBot.execute(message);
+        System.out.println(response.isOk());
+        System.out.println(response.errorCode());
     }
 
 }
